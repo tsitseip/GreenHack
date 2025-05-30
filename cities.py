ff344d51777ba314ffc7f56aeb60ab8174d7638f
Vasyl Diakulych <vasyadiakulych@gmail.com>
2025-05-31 00:39:11 +0200

    added cities

---
+++ cities.py
@@ -0,0 +1,12 @@
+import pickle
+
+cities = set()
+
+with open('dictEm.pkl', 'rb') as fp:
+    cachedDict = pickle.load(fp)
+    for i in range(len(cachedDict['Subsidiary'])):
+        cities.add(cachedDict['Location'][i])
+        cities.add(cachedDict['Trip departure'][i])
+
+with open('cities.pkl', 'wb') as fp:
+    pickle.dump(set, fp)
