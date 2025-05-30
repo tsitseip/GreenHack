0b237446e25b0844e0b0b9942f1ca07e894fda6d
tsitseip <cicejp@gmail.com>
2025-05-30 19:01:06 +0200

    First commit

---
+++ emmision.py
@@ -0,0 +1,6 @@
+import pickle
+
+with open('dict.pkl', 'rb') as fp:
+    table = pickle.load(fp)
+
+
---
+++ parser.py
@@ -0,0 +1,50 @@
+with open('data_parsed.csv', 'r') as f:
+    line = f.readline()
+    # print(line)
+    columns = []
+    name = ""
+    started = False
+    for c in line:
+        if c=='\"':
+            started = not started
+        else:
+            if started == True or c != ',':
+                name += c
+            else:
+                if c==',':
+                    columns.append(name)
+                    name = ""
+    if name != "":
+        columns.append(name[:-1])
+        name = ""
+    # print(columns)
+
+    dict = {}
+    for i in columns:
+        dict[i] = []
+
+    for line in f:
+        name = ""
+        started = False
+        i = 0
+        for c in line:
+            if c=='\"':
+                started = not started
+            else:
+                if started == True or c != ',':
+                    name += c
+                else:
+                    if c==',':
+                        dict[columns[i]].append((name, i))
+                        name = ""
+                        i+=1
+        if name != "":
+            dict[columns[i]].append((name[:-1], i))
+            name = ""
+
+import pickle
+
+with open('dict.pkl', 'wb') as fp:
+    pickle.dump(dict, fp)
+
+
